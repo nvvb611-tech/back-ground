cedd1a9af66dbb97a9f78cf30a7dc7dcd41f7535
nvvb611-tech <nvvb611@gmail.com>
2025-10-24 22:08:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const Button=document.getElementById("btn");
+const colortext=document.querySelector(".color");
+const hex=['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+Button.addEventListener("click",()=>{
+    const hexcolor =generatehexcolor();
+    document.body.style.backgroundColor=hexcolor;
+    colortext.textContent=hexcolor;
+}
+);
+function generatehexcolor(){
+    let hexcolor="#"
+    for(i=0;i<6;i++){
+        hexcolor=hexcolor+hex[generateRandomNumber()]
+    }
+    return hexcolor;
+ };
+ function generateRandomNumber(){
+    return Math.floor(Math.random()*hex.length)
+ };
+
+
+
